64437ea65c5b04e170e9e757116c8c198a985e67
Alexandros Genzor <agenzors@campusdigitalfp.com>
2024-11-22 20:02:58 +0100

    added new bullet prefab. added shoot logic to player to shoot the new bullet prefab.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -12,15 +12,20 @@ public class PlayerBehaviour : MonoBehaviour
     public float DistanceToGround = 0.1f;
     public LayerMask GroundLayer;
 
+    public GameObject Bullet;
+    public float BulletSpeed = 100f;
+
 
     private float _vInput;
     private float _hInput;
 
     private bool _isJumping;
 
+    private Rigidbody _rigidBody;
+
     private CapsuleCollider _capCol;
 
-    private Rigidbody _rigidBody;
+    private bool _isShooting;
     // Start is called before the first frame update
     void Start()
     {
@@ -43,6 +48,8 @@ public class PlayerBehaviour : MonoBehaviour
         this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
         //*/
 
+        _isShooting |= Input.GetKeyDown(KeyCode.Space);
+
     }
 
     private void FixedUpdate()
@@ -50,7 +57,8 @@ public class PlayerBehaviour : MonoBehaviour
         Vector3 rotation = Vector3.up * _hInput;
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
 
-        _rigidBody.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
+        _rigidBody.MovePosition(this.transform.position + 
+            this.transform.forward * _vInput * Time.fixedDeltaTime);
         _rigidBody.MoveRotation(_rigidBody.rotation * angleRot);
 
 
@@ -61,13 +69,27 @@ public class PlayerBehaviour : MonoBehaviour
         }
 
         _isJumping = false;
+
+        if (_isShooting)
+        {
+            GameObject newBullet = Instantiate(Bullet, this.transform.position +
+                new Vector3(0, 0, 1), this.transform.rotation);
+
+            Rigidbody BulletRB = newBullet.GetComponent<Rigidbody>();
+            BulletRB.velocity = this.transform.forward * BulletSpeed;
+
+        }
+
+        _isShooting = false;
         
     }
 
     private bool IsGrounded()
     {
-        Vector3 capsuleBottom = new Vector3(_capCol.bounds.center.x, _capCol.bounds.min.y, _capCol.bounds.center.z);
-        bool grounded = Physics.CheckCapsule(_capCol.bounds.center, capsuleBottom, DistanceToGround, GroundLayer, QueryTriggerInteraction.Ignore);
+        Vector3 capsuleBottom = new Vector3(_capCol.bounds.center.x, 
+            _capCol.bounds.min.y, _capCol.bounds.center.z);
+        bool grounded = Physics.CheckCapsule(_capCol.bounds.center, capsuleBottom, 
+            DistanceToGround, GroundLayer, QueryTriggerInteraction.Ignore);
 
         return grounded;
 
